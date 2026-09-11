d16065a99b4c876e847b279d6b1fc4af9712d7d0
FarhanK29 <farhan.k@ibm.com>
2026-09-11 09:16:42 -0400

    fixed comments

--- components.tfcomponent.hcl
+++ components.tfcomponent.hcl
@@ -3,7 +3,7 @@ required_providers {
     source  = "hashicorp/null"
     version = "~> 3.2"
   }
-  #Comment out this block
+  # Comment out this block
   # random = {
   #   source  = "hashicorp/random"
   #   version = "~> 3.9"
@@ -42,7 +42,7 @@ output "ids" {
   value = component.test.ids
 }
 
-#comment this block out
+# comment this block out
 # output "random_names" {
 #   type  = list(string)
 #   value = component.test.random_names
--- components/test/main.tf
+++ components/test/main.tf
@@ -4,7 +4,7 @@ terraform {
       source  = "hashicorp/null"
       version = "~> 3.2"
     }
-    #Comment this block out
+    # Comment this block out
     # random = {
     #   source  = "hashicorp/random"
     #   version = "~> 3.9"
@@ -27,7 +27,7 @@ resource "null_resource" "test" {
   count    = var.resource_count
 }
 
-#comment out this block
+# comment out this block
 # resource "random_pet" "test" {
 #   count  = 3
 #   prefix = var.deployment_name
@@ -37,7 +37,7 @@ output "ids" {
   value = null_resource.test[*].id
 }
 
-#comment out this block
+# comment out this block
 # output "random_names" {
 #   value = random_pet.test[*].id
 # }
